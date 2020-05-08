949983438e932e0eaf73dc430a98fcf1c3ddc6f1
Ryan <devdplx@gmail.com>
2020-05-08 14:01:39 -0700

    Rework to discord.js-commando

--- bot.js
+++ bot.js
@@ -1,29 +1,28 @@
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+//
+const { Commando } = require('discord.js-commando');
+const Path = require('path');
+//
+const Oracle = new Commando({
+    commandPrefix: '>',
+    owner: ''
+});
+//
+Oracle.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['first', 'Your First Command Group'],
+		['second', 'Your Second Command Group'],
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+//
+Oracle.once('ready', () => {
+	console.log(`Logged in as ${Oracle.user.tag}! (${Oracle.user.id})`);
+	Oracle.user.setActivity('with Commando');
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+Oracle.on('error', console.error);
+//
+Oracle.login(process.env.BOT_TOKEN);
+//
