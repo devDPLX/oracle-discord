d8a5e920e72eea499ae78ebe74c233abe148a58b
Ryan <devdplx@gmail.com>
2020-05-08 14:15:28 -0700

    Testing

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 //
 const { Commando } = require('discord.js-commando');
 const Path = require('path');
+console.log('1');
 //
 const Oracle = new Commando({
     commandPrefix: '>',
     owner: ''
 });
+console.log('2');
 //
 Oracle.registry
 	.registerDefaultTypes()
@@ -17,12 +19,14 @@ Oracle.registry
 	.registerDefaultCommands()
 	.registerCommandsIn(path.join(__dirname, 'commands'));
 //
+console.log('3');
 Oracle.once('ready', () => {
 	console.log(`Logged in as ${Oracle.user.tag}! (${Oracle.user.id})`);
 	Oracle.user.setActivity('with Commando');
 });
-
+console.log('4');
 Oracle.on('error', console.error);
 //
 Oracle.login(process.env.BOT_TOKEN);
+console.log('5');
 //
