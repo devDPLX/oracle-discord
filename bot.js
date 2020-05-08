6a1341ff41b9720d954f08ebe4896d2848b0b2c9
Ryan <devdplx@gmail.com>
2020-05-08 15:54:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,29 @@
-console.log('0');
-const { Commando } = require('discord.js-commando');
-const Path = require('path');
-console.log('1');
-//
-const Oracle = new Commando({
-    commandPrefix: '>',
-    owner: ''
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
-console.log('2');
-//
-Oracle.registry
-	.registerDefaultTypes()
-	.registerGroups([
-		['first', 'Your First Command Group'],
-		['second', 'Your Second Command Group'],
-	])
-	.registerDefaultGroups()
-	.registerDefaultCommands()
-	.registerCommandsIn(path.join(__dirname, 'commands'));
-//
-console.log('3');
-Oracle.once('ready', () => {
-	console.log(`Logged in as ${Oracle.user.tag}! (${Oracle.user.id})`);
-	Oracle.user.setActivity('with Commando');
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
-console.log('4');
-Oracle.on('error', console.error);
-//
-Oracle.login(process.env.BOT_TOKEN);
-console.log('5');
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
